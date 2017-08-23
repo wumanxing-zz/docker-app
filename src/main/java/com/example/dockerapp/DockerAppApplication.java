43db7e222140bc2e9dd6aa4d72680c8a1c03f906
wumanxing <wumanxing123@163.com>
2017-08-23 14:10:50 +0800

    Add files via upload

---
+++ src/main/java/com/example/dockerapp/DockerAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.dockerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerAppApplication.class, args);
+	}
+}
